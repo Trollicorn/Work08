5797fcd3e69f5736d0e0ca94563f84479872ee55
Trollicorn <muddin3@stuy.edu>
2018-10-22 17:04:47 -0400

    bitwise change

--- sieve.c
+++ sieve.c
@@ -5,27 +5,32 @@
 
 
 int sieve(int targetPrime){
-	int size = targetPrime * log(targetPrime) * 1.15;
-	char * nums = calloc(size,sizeof(char));
+	int size = targetPrime * log(targetPrime) * 1.15 / 4;
+	int bits = size * (int)(sizeof(int));
+	int * nums = calloc(size,sizeof(int));
 	int pr = 0;
 	int cur;
-//	printf("%d\n",size );
-	for (int i = 2; i < size; i++){
+//	printf("%d\n",bits );
+	for (int bit = 2; bit < bits; bit++){
 	//	printf("i=%d, nums[i]=%d, pr=%d\n",i,nums[i],pr );
-		if (!nums[i]){
+		if (! ((nums[bit/32] >> (bit%32)) & 1)){
 			pr++;
 		//	printf("%d is %d\n", pr, nums[i]);
 			if (pr == targetPrime){
-				return i;
+				return bit;
 			}
-			cur = i;
-			for (int j = i; j < size; j += cur){
-				nums[j] = 'a';
+			cur = bit;
+			for (int j = bit; j < bits; j += cur){
+				nums[bit/32] |= 1 << (bit%32);
 			}
 		}
 	}
 	return -1;
 }
+/*
+void setBit(int[] A, int k){
+	A[k/32] |= 1 << (k%32);
+}*/
 /*
 int main(){
 	printf("%d\n",sieve(100000) );
